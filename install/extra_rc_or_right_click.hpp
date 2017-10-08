351fb371010fd35cc2a7a889e54e48e69c26555c
Michael Allen <allen.michaeld@gmail.com>
2017-10-08 01:33:38 -0400

    TYPO!!

--- install/extra_rc_or_right_click.hpp
+++ install/extra_rc_or_right_click.hpp
@@ -10,7 +10,7 @@ class ExtraRc {
   class ItemPickAxe {
     class Remove_Road {
       text = "Remove Road";
-      script = "closeDialog 0;execVM 'scripts\buildables\doadDismantle.sqf';";
+      script = "closeDialog 0;execVM 'scripts\buildables\roadDismantle.sqf';";
     };
   };	
 };
@@ -18,6 +18,6 @@ class ExtraRc {
 if you want to use click actions, then add this to your config
 
 ["ItemToolbox","Crafting Menu","closeDialog 0;createDialog ""ZCraft"";","true"],
-["ItemPickAxe","Remove Road","closeDialog 0;execVM 'scripts\buildables\doadDismantle.sqf';","true"]
+["ItemPickAxe","Remove Road","closeDialog 0;execVM 'scripts\buildables\roadDismantle.sqf';","true"]
 
 if you want to use a keybind then that is on you, just call the scripts from their right locations
